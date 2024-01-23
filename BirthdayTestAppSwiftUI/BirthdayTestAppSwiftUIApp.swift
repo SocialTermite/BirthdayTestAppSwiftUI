0f149a779b988b7d9a02ba871c0ccfd8185007c7
Konstantin Bondar <bondar.k.dev@gmail.com>
2024-01-23 09:18:06 +0100

    Initial Commit

---
+++ BirthdayTestAppSwiftUI/BirthdayTestAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayTestAppSwiftUIApp.swift
+//  BirthdayTestAppSwiftUI
+//
+//  Created by Konstantin Bondar on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayTestAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
