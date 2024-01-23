68b8db10a23ea1f52aec35a13d1ad7331c4b1ef0
Konstantin Bondar <bondar.k.dev@gmail.com>
2024-01-23 22:38:26 +0100

    Create birthday view with 3 styles and dynamic header(not refactored to clean state)

---
+++ BirthdayTestAppSwiftUI/Screens/BirthdayView/BirthdayView.swift
@@ -0,0 +1,8 @@
+//
+//  BirthdayView.swift
+//  BirthdayTestAppSwiftUI
+//
+//  Created by Konstantin Bondar on 23.01.2024.
+//
+
+import Foundation
---
+++ BirthdayTestAppSwiftUI/Services/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  BirthdayTestAppSwiftUI
+//
+//  Created by Konstantin Bondar on 23.01.2024.
+//
+
+import Foundation
